cd45954d630e9046e0ae4b74998b3fbbbde25ba9
liwen <952799980@qq.com>
2025-09-08 21:05:44 +0800

    feat:update

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -295,7 +295,7 @@ class Scheduler(
             self.recv_from_tokenizer = None
             self.recv_from_rpc = None
             self.send_to_tokenizer = SimpleNamespace(send_pyobj=lambda x: None)
-            self.send_to_detokenizer = SimpleNamespace(send_pyobj=lambda x: None)
+            self.send_to_detokenizer = [SimpleNamespace(send_pyobj=lambda x: None)]
 
         if self.current_scheduler_metrics_enabled():
             self.send_metrics_from_scheduler = get_zmq_socket(
