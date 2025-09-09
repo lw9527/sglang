a6f51cea88d13fb9149796d96115c9398e2ea39d
liwen <952799980@qq.com>
2025-09-09 09:33:07 +0800

    feat:update

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2398,10 +2398,11 @@ class Scheduler(
         return result
 
     def register_multi_tokenizer(self, recv_req: MultiTokenizerRegisterReq):
-        worker_id = get_worker_ids_from_req_rids(recv_req.rids)[0]
-        detokenizer_index = worker_id % self.server_args.detokenizer_worker_num
-        # for i in range(self.server_args.detokenizer_worker_num):
-        self.send_to_detokenizer[detokenizer_index].send_pyobj(recv_req)
+        #worker_id = get_worker_ids_from_req_rids(recv_req.rids)[0]
+        #detokenizer_index = worker_id % self.server_args.detokenizer_worker_num
+        for i in range(self.server_args.detokenizer_worker_num):
+            self.send_to_detokenizer[i].send_pyobj(recv_req)
+        #self.send_to_detokenizer[detokenizer_index].send_pyobj(recv_req)
         return recv_req
 
     def slow_down(self, recv_req: SlowDownReqInput):
@@ -2465,7 +2466,9 @@ class Scheduler(
     def handle_freeze_gc(self, recv_req: FreezeGCReq):
         """Handle freeze_gc request: freeze scheduler's GC and forward to detokenizer."""
         freeze_gc("Scheduler")
-        self.send_to_detokenizer.send_pyobj(recv_req)
+        for detokenizer in self.send_to_detokenizer:
+            detokenizer.send_pyobj(recv_req)
+        #self.send_to_detokenizer.send_pyobj(recv_req)
         return None
 
 
